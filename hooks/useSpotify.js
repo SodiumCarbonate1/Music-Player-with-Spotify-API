b6d543d812beab3efc0f991b4960b5527c60e9c3
Qingyuan Chen <Qingyuan.Chen2@georgebrown.ca>
2022-02-23 18:39:47 -0500

    music player

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,23 +1,5 @@
 import { signIn, useSession } from "next-auth/react"
 import { useEffect } from "react";
-
-// import spotifyApi from '../lib/spotify';
-
-// function useSpotify() {
-//     const {data:session, status} = useSession();
-//     useEffect(() => {
-//         if(session){
-//             if(session.error === "RefreshAccessTokenError"){
-//                 signIn();
-//             }
-//             spotifyApi.setAccessToken(session.user.accessToken);
-//         }
-//     }, [session]);
-//     return spotifyApi;
-// }
-// export default useSpotify;
-
-
 import SpotifyWebApi from "spotify-web-api-node";
 const spotifyApi  = new SpotifyWebApi({
     clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
